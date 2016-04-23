dff724c1389e2ac5d1827aaba4d3c422b1254b97
Zhigang Fang <zhigang1992@gmail.com>
2016-04-24 01:14:12 +0800

    Add Todo Reducer

--- MyPlayground.playground/Pages/Demo.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Demo.xcplaygroundpage/Contents.swift
@@ -9,3 +9,12 @@ struct Todo {
 enum Action {
     case AddTodo(String)
 }
+
+let addTodo: ([Todo], Action) -> [Todo] = { todos, action in
+    guard case .AddTodo(let name) = action else { return todos }
+    let newTodo = Todo(id: todos.count, name: name, completed: false)
+    return todos + [newTodo]
+}
+
+let state = [Todo]()
+print(addTodo(state, .AddTodo("Hello, World")))
